b8ba3f4c45e2095cd5f3730113972da14c37986b
ParkSeonWoo820 <shop6433@naver.com>
2020-02-20 20:11:43 +0900

    재시작

---
+++ app/src/main/java/com/MyFirstMapNaver/Map/Intro.java
@@ -0,0 +1,24 @@
+package com.MyFirstMapNaver.Map;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Intro extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro);
+        new Thread(){
+            public void run(){
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent i = new Intent(Intro.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }.start();
+    }
+}
