e4d007abc9fb38330eb27c5ce946c47f953874cd
ParkSeonWoo820 <shop6433@naver.com>
2020-02-21 18:06:57 +0900

    메인엑티비티수정

--- app/src/main/java/com/MyFirstMapNaver/Map/MainActivity.java
+++ app/src/main/java/com/MyFirstMapNaver/Map/MainActivity.java
@@ -2,12 +2,13 @@ package com.MyFirstMapNaver.Map;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mainactivity);
     }
 }
