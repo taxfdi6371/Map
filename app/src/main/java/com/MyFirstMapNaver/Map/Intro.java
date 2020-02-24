91a244ea41a44c27e3dc1019eb0198e77fb49018
ParkSeonWoo820 <shop6433@naver.com>
2020-02-24 22:18:35 +0900

    현위치 추가

--- app/src/main/java/com/MyFirstMapNaver/Map/Intro.java
+++ app/src/main/java/com/MyFirstMapNaver/Map/Intro.java
@@ -1,22 +1,40 @@
 package com.MyFirstMapNaver.Map;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class Intro extends Activity {
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro);
-        new Thread(){
-            public void run(){
+
+        new Thread() {
+            public void run() {
                 try {
                     Thread.sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                Intent i = new Intent(Intro.this,MapsActivity.class);
-                startActivity(i);
+//GPS화면 띄우기 인데 여기 잘 모르겠음
+                LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+                boolean enabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+                if (!enabled) {
+                    Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(i);
+                } //여기가 헷갈림
+                    Intent i = new Intent(Intro.this, MapsActivity.class);
+                    startActivity(i);
                 finish();
             }
         }.start();
--- app/src/main/java/com/MyFirstMapNaver/Map/MapsActivity.java
+++ app/src/main/java/com/MyFirstMapNaver/Map/MapsActivity.java
@@ -1,34 +1,65 @@
 package com.MyFirstMapNaver.Map;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.LocationOverlay;
 import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.util.FusedLocationSource;
 
 public class MapsActivity extends Activity implements OnMapReadyCallback {
+    private final int MY_PERMISSIONS_REQUEST_LOCATION = 1001;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         MapView mapView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mapsactivity);
         mapView = findViewById(R.id.map_view);
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
         mapView.getMapAsync(this);
 
-
-
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,  @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            return;
+        }
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
-        Marker marker = new Marker();
-        marker.setPosition(new LatLng(37.5670135, 126.9783740));
-        marker.setMap(naverMap);
-    }
+    Marker marker = new Marker();
+    LatLng MyLatLng = new LatLng(35.888,128.65);
+   // CameraPosition cameraPosition = new CameraPosition(MyLatLng, 14, 0,  0 );
+    marker.setPosition(MyLatLng);
+
+    naverMap.setLocationSource(locationSource);
+        //Toast.makeText(this,""+naverMap.getMapType(),Toast.LENGTH_LONG).show();//지도의 타입
+    marker.setMap(naverMap);
+    //marker2.setPosition((new LatLng(35.88,128.655))); marker2.setMap(naverMap);
+}
+
 }
